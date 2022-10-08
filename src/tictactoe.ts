229869b4ae890610b319cdfd5fee884314509897
Filip Brouwers <68373920+Fil-good@users.noreply.github.com>
2022-10-08 21:15:46 +0200

    Update tictactoe.ts
    manual update through gh website

--- src/tictactoe.ts
+++ src/tictactoe.ts
@@ -1,19 +1,18 @@
 
-// make board class ? with properties type, but 1 board per game
+// git comit is with one T !!!! so pushed to remote before comiting :-S
 
 class Board {
   public board: string[][] = [["1", "2", "3"],
                               ["4", "5", "6"],
                               ["7", "8", "9"]]; // 'string array' [], dus array van string arrays
 
-
   // update and render board
-  public renderBoard (board: string[][], line: string, currentPlayer: string) {
+  public renderBoard(board: string[][], line: string, currentPlayer: string) {
 
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if(board[i][j]==line) {
-          board[i][j]=currentPlayer;
+        if (board[i][j] == line) {
+          board[i][j] = currentPlayer;
         }
         process.stdout.write(board[i][j]);
       }
@@ -21,30 +20,27 @@ class Board {
     }
   }
 
-  public checkIfWinner(board: string[][]) {
-
+  public checkIfWinner(board1: string[][]) {
     let winnerCombos = [
-      [[0][0], [0][1], [0][2]],
-      [[1][0],[1][1],[1][2]],
-      [[2][0], [2][1], [2][2]],
-      [[0][0], [1][0], [2][0]],
-      [[0][1], [1][1], [2][1]],
-      [[0][2], [1][2], [2][2]],
-      [[0][0], [1][1], [2][2]],
-      [[0][2], [1][1], [2][0]]
+      [[0, 0], [0, 1], [0, 2]], /* per element van de array, de 2 indexen */
+      [[1, 0], [1, 1], [1, 2]],
+      [[2, 0], [2, 1], [2, 2]],
+      [[0, 0], [1, 0], [2, 0]],
+      [[0, 1], [1, 1], [2, 1]],
+      [[0, 2], [1, 2], [2, 2]],
+      [[0, 0], [1, 1], [2, 2]],
+      [[0, 2], [1, 1], [2, 0]]
     ];
-
-    for (let [i1, i2, i3] of winnerCombos) {
-      if(board[i1] === board[i2] &&
-        board[i1] === board[i3]) {
-        return true;
+    // walk through the combos
+    for (let [i1, i2, i3] of winnerCombos) { // i1 = [[0,0],..]
+      if (board1[i1[0]][i1[1]].toString() === board1[i2[0]][i2[1]].toString() &&
+        board1[i1[0]][i1[1]].toString() === board1[i3[0]][i3[1]].toString()) {
+        // console.log("winner");
+        game.isGameOver = true;
+        break;
       }
     }
-    return false;
-
-
   }
-
 }
 
 class Game extends Board {
@@ -57,14 +53,14 @@ class Game extends Board {
 
     this.renderBoard(this.board, line, this.currentPlayer);
 
-    (this.currentPlayer=="X") ? this.currentPlayer="O" : this.currentPlayer="X";
+    (this.currentPlayer == "X") ? this.currentPlayer = "O" : this.currentPlayer = "X";
 
   }
 
   public command(line: string) {
 
-    if(this.isGameOver != true) {
-      process.stdout.write("go on!");
+    if (this.isGameOver != true) {
+      process.stdout.write(`you putted ${line}!`);
     }
   }
 }
